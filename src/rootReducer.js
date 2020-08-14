7c67f31025062066473d4f4e49719ea27bd37596
heyimmatt <mrmcfarland@gmail.com>
2020-08-14 16:36:25 -0700

    Update code formatting

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,14 +1,14 @@
 const INITIAL_STATE = { memes: [], deadMemes: [] };
 
 export default function rootReducer(state = INITIAL_STATE, action) {
-  const {type, payload} = action;
+  const { type, payload } = action;
   switch (type) {
-    case "ADD_MEME":
-      return {...state, memes: [...state.memes, payload]}
-    case "DELETE_MEME":
-      const newMemeArr = [...state.memes]
-      newMemeArr.splice(payload, 1)
-      return {...state, memes: [...newMemeArr]}
+    case 'ADD_MEME':
+      return { ...state, memes: [...state.memes, payload] };
+    case 'DELETE_MEME':
+      const newMemeArr = [...state.memes];
+      newMemeArr.splice(payload, 1);
+      return { ...state, memes: [...newMemeArr] };
     default:
       return state;
   }
