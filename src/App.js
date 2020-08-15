585ade1826a020a8560b1191877d827c39d5e088
heyimmatt <mrmcfarland@gmail.com>
2020-08-15 11:06:42 -0700

    Implement add and delete todo functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import React, { useState } from 'react';
 import { Row, Col, Container } from 'reactstrap';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch} from 'react-redux';
+import TodoForm from './TodoForm';
+import Todos from './Todos';
 
 export default function App() {
+  const INITIAL_FORM_STATE = {
+    todoItem: ''
+  };
+  const [formData, setFormData] = useState(INITIAL_FORM_STATE);
+  const dispatch = useDispatch();
+
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setFormData((fdata) => ({
+      ...fdata,
+      [name]: value,
+    }));
+  };
+  
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch({type: 'ADD_TODO', payload: {...formData}});
+    setFormData(INITIAL_FORM_STATE);
+  }
+
+  const deleteTodo = (e) => {
+    const todoIdx = parseInt(e.target.parentElement.id);
+    dispatch({type: 'DELETE_TODO', payload: todoIdx})
+  }
+
+
   return (
     <Container>
       <Row className="align-items-center">
         <Col className="text-center">
           <h1>Todo List</h1>
         </Col>
+        <TodoForm formData={formData} changeHandler={changeHandler} submitHandler={submitHandler} />
+        <Todos deleteTodo={deleteTodo} />
       </Row>
     </Container>
   );
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -4,17 +4,12 @@ export default function rootReducer(state = INITIAL_STATE, action) {
   const { type, payload } = action;
   switch (type) {
     case 'ADD_TODO':
-      return { ...state, memes: [...state.memes, payload] };
+      return { ...state, todoList: [...state.todoList, payload] };
 
-    // case 'DELETE_MEME':
-    //   const newMemeArr = [...state.memes];
-    //   newMemeArr.splice(payload, 1);
-    //   return { ...state, memes: [...newMemeArr] };
-
-    // case 'EDIT_MEME':
-    //   const updateMemeArr = [...state.memes];
-    //   updateMemeArr.splice(action.memeId, 1, payload);
-    //   return { ...state, memes: [...updateMemeArr] };
+    case 'DELETE_TODO':
+      const newTodoArr = [...state.todoList];
+      newTodoArr.splice(payload, 1);
+      return { ...state, todoList: [...newTodoArr] };
 
     default:
       return state;
